ea4e64e0b0d825a40740a072990c9b37f66242e1
abaines <abaines@shopashley.com>
2025-10-10 11:39:39 -0400

    small fix 3 for Post

--- blog/models.py
+++ blog/models.py
@@ -8,7 +8,7 @@ from django.contrib.auth.models import User
 class Post(models.Model):
 
     #Links author to the user or superuser
-    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     text = models.TextField()
     #Uses timezone 
